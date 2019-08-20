93eb7cc9946058764f86cfc686b55af9bb148a38
Andre <NodePoint@users.noreply.github.com>
2019-08-20 18:40:47 +0100

    Improved regular expression

--- cross-ref.py
+++ cross-ref.py
@@ -16,38 +16,39 @@ dumpbmem = []
 matched = []
 unmatched = []
 
-re_pattern = re.compile(r'[\w\.-]+@[\w\.-]+')
+re_pattern = re.compile(r'[\w\.+-]+@[\w\.-]+')
 
-print (' ')
-print ('DATA BREACH CROSS-REFERENCE TOOL')
-print (' ')
-print ('FILE A (TO USE): '+dumpa)
-print ('FILE B (TO CROSS-REFERENCE): '+dumpb)
-print (' ')
-print ('!!! Depending on the file size(s), this might take a while. !!!')
-print (' ')
-print ('Extracting email(s) from '+dumpa+' ...')
+print(' ')
+print('DATA BREACH CROSS-REFERENCE TOOL')
+print(' ')
+print('FILE A (TO USE): '+dumpa)
+print('FILE B (TO CROSS-REFERENCE): '+dumpb)
+print(' ')
+print('!!! Depending on the file sizes, this might take a while. !!!')
+print(' ')
+print('Extracting email(s) from '+dumpa+' ...')
 
 # DUMP A
-with codecs.open(dumpa,'r',encoding='utf8') as fh_in:    
+with codecs.open(dumpa,'r', encoding='utf8') as fh_in:    
     for linea in fh_in:
         match_lista = re_pattern.findall(linea)
         if match_lista:
             dumpamem.append(match_lista[0])
             gatheredacca += 1
 
-print (str(gatheredacca)+' email(s) loaded !')
-print ('Extracting email(s) from '+dumpb+' ...')
+print(str(gatheredacca)+' email(s) loaded !')
+print('Extracting email(s) from '+dumpb+' ...')
 
 # DUMP B
-with codecs.open(dumpb,'r',encoding='utf8') as f:
+with codecs.open(dumpb,'r', encoding='utf8') as f:
     for lineb in f:
         match_listb = re_pattern.findall(lineb)
         if match_listb:
             dumpbmem.append(match_listb[0])
             gatheredaccb += 1
-print (str(gatheredaccb)+' email(s) loaded !')
-print ('Verifying ...')
+
+print(str(gatheredaccb)+' email(s) loaded !')
+print('Verifying ...')
 
 # REF STAGE
 for dumpadat in dumpamem:
@@ -58,8 +59,6 @@ for dumpadat in dumpamem:
         unmatched.append(dumpadat)
         invalidacc += 1
 
-print (' ')
-print ('Valid account(s): '+str(validacc))
-print ('Invalid account(s): '+str(invalidacc))
-
-# TODO: duplicate check -- duplicates currently go under "invalid" account(s)
+print(' ')
+print('Valid account(s): '+str(validacc))
+print('Invalid account(s): '+str(invalidacc))
